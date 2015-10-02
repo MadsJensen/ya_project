a2404dff40d3c0078a99b128a1c18f37dbf91471
Mads Jensen <mje.mads@gmail.com>
2015-10-02 19:54:59 +0200

    Updated to allow for missing sequences

--- cluster_recon_all.py
+++ cluster_recon_all.py
@@ -28,56 +28,57 @@ subjects_dir = os.path.join(scratch_folder, 'fs_subjects_dir')
 script_dir = proj_folder+'/scripts/MR_scripts'
 
 included_subjects = db.get_subjects()
+# included_subjects = included_subjects[3:]
 # just test with first one!
 # included_subjects = [included_subjects[1]]
 
 for sub in included_subjects:
 
     # this is an example of getting the DICOM files as a list
-    sequence_name = 't1_mp2rage_sag_p2_iso_UNI_Images_ND'
+    sequence_name = 't1_mp2rage_sag_p2_iso_UNI_Images'
     mr_study = db.get_studies(sub, modality='MR', unique=True)
     if mr_study is not None:
         # This is a 2D list with [series_name, series_number]
         series = db.get_series(sub, mr_study[0], 'MR')
     # Change this to be more elegant: check whether any item in series
     # matches sequence_name
-    T1_file_names = db.get_files(sub, mr_study[0], 'MR',
-                                 series[sequence_name])
+        T1_file_names = db.get_files(sub, mr_study[0], 'MR',
+                                    series[sequence_name])
 
-    input_dicom = T1_file_names[0]  # first DICOM file
+        input_dicom = T1_file_names[0]  # first DICOM file
 
-    bash_script = ['#!/usr/bin/env bash']
-    bash_script.append('#$ -S /bin/bash')  # for qsub
+        bash_script = ['#!/usr/bin/env bash']
+        bash_script.append('#$ -S /bin/bash')  # for qsub
 
-    # have to explicitly source bashrc (non-login shell)
-    bash_script.append('source ~/.bashrc')
-    bash_script.append("export FREESURFER_HOME=/usr/local/freesurfer")
-    bash_script.append("source $FREESURFER_HOME/SetUpFreeSurfer.sh")
-    # bash_script.append('use mne') # set paths
-    bash_script.append('export SUBJECTS_DIR=' + subjects_dir)
+        # have to explicitly source bashrc (non-login shell)
+        bash_script.append('source ~/.bashrc')
+        bash_script.append("export FREESURFER_HOME=/usr/local/freesurfer")
+        bash_script.append("source $FREESURFER_HOME/SetUpFreeSurfer.sh")
+        # bash_script.append('use mne') # set paths
+        bash_script.append('export SUBJECTS_DIR=' + subjects_dir)
 
-    # bash_script.append('export SUBJECT=' + sub[:4])
+        # bash_script.append('export SUBJECT=' + sub[:4])
 
-    bash_script.append('recon-all -s ' + sub[:4] + "_ND" + ' -i '
-                        + input_dicom + ' -all')
-    # Can do some exit value checking too...
-    # bash_script.append('if [[ $? != 0 ]] ; then exit 1; fi')
+        bash_script.append('recon-all -s ' + sub[:4] + "_ND" + ' -i '
+                            + input_dicom + ' -all')
+        # Can do some exit value checking too...
+        # bash_script.append('if [[ $? != 0 ]] ; then exit 1; fi')
 
-    # we could just write each line directly into the script_file
-    # instead of first generating the bash_script list
-    # but it may be beneficial at some stage to /first/ write all
-    # the scripts and only then submit all of them at once
-    os.chdir(script_dir)
-    script_name = 'sub_recon_' + sub + '.sh'
-    script_file = open(script_name, 'wt')
-    for item in bash_script:
-        script_file.write("%s\n" % item)
-    script_file.close()
+        # we could just write each line directly into the script_file
+        # instead of first generating the bash_script list
+        # but it may be beneficial at some stage to /first/ write all
+        # the scripts and only then submit all of them at once
+        os.chdir(script_dir)
+        script_name = 'sub_recon_' + sub + '.sh'
+        script_file = open(script_name, 'wt')
+        for item in bash_script:
+            script_file.write("%s\n" % item)
+        script_file.close()
 
-#     process = subprocess.Popen(["chmod u+x " + script_name], shell=True)
-#     process.communicate()
-#      # remove the echo " ... " to make this run for real...
-#     qsub_cmd = '"qsub -j y -q long.q ' + script_name + '"'
-# #    qsub_cmd = ["qsub -j y -q long.q ", script_name]
-#     process = subprocess.Popen([qsub_cmd], shell=True)
-#     process.communicate()
+    #     process = subprocess.Popen(["chmod u+x " + script_name], shell=True)
+    #     process.communicate()
+    #      # remove the echo " ... " to make this run for real...
+    #     qsub_cmd = '"qsub -j y -q long.q ' + script_name + '"'
+    # #    qsub_cmd = ["qsub -j y -q long.q ", script_name]
+    #     process = subprocess.Popen([qsub_cmd], shell=True)
+    #     process.communicate()
