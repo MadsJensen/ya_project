37af2faeed00f87962736114eb49cedf78b8b136
Mads Jensen <mje.mads@gmail.com>
2016-06-28 14:22:39 +0200

    fixes

--- rest_analysis/copy_mp2rage.sh
+++ rest_analysis/copy_mp2rage.sh
@@ -1,12 +1,12 @@
 
 
-orig_dir = "/projects/MINDLAB2015_MR-YoungAddiction/scratch/bgfixed"
-data_dir = "/projects/MINDLAB2015_MR-YoungAddiction/scratch/rest_analysis_non-lin"
+orig_dir="/projects/MINDLAB2015_MR-YoungAddiction/scratch/bgfixed"
+data_dir="/projects/MINDLAB2015_MR-YoungAddiction/scratch/rest_analysis_non-lin"
 
-cd orig_dir
+cd $orig_dir
 
 for file in *.nii
 do
-    echo cp file data_dir/${1: 0:4}/mp2rage.nii
+    echo cp file $data_dir/${1: 0:4}/mp2rage.nii
     # $script_dir/convert_dicom.sh $file
 done
