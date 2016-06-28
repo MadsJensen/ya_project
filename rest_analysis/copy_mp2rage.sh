492d7ab189d03235891cf18dff6bf6b4a972a0f7
Mads Jensen <mje.mads@gmail.com>
2016-06-28 14:28:21 +0200

    working?

--- rest_analysis/copy_mp2rage.sh
+++ rest_analysis/copy_mp2rage.sh
@@ -7,6 +7,6 @@ cd $orig_dir
 
 for file in *.nii
 do
-    echo cp $file $data_dir/${file: 0:4}/mp2rage.nii
+    cp $file $data_dir/${file: 0:4}/mp2rage.nii
     # $script_dir/convert_dicom.sh $file
 done
