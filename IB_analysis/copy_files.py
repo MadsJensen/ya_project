ebb90a2284718dada709eb2b24c47844dd0d26fb
Mads Jensen <au194693@pc-10-3-149-58.pet.auh.dk>
2016-11-02 10:41:29 +0100

    more files

--- IB_analysis/copy_files.py
+++ IB_analysis/copy_files.py
@@ -2,7 +2,7 @@ import os
 import glob
 from shutil import copyfile
 
-root_dir = "/projects/MINDLAB2015_MR-YoungAddiction/misc/behavioural"
+root_dir = "/projects/MINDLAB2015_MR-YoungAddiction/misc/behavioural/"
 target_dir = "/projects/MINDLAB2015_MR-YoungAddiction/" \
              "scratch/MJ/IB_analysis/data"
 
@@ -15,5 +15,20 @@ subjects.sort()
 for subject in subjects:
     src = root_dir + subject + "/%s_singleTone1.csv" % subject
     dst = target_dir + "/%s_singleTone1.csv" % subject
+    print(src)
+    print(dst)
+
+    src = root_dir + subject + "/%s_singlePress.csv" % subject
+    dst = target_dir + "/%s_singlePress.csv" % subject
+    print(src)
+    print(dst)
+
+    src = root_dir + subject + "/%s_actionTone1.csv" % subject
+    dst = target_dir + "/%s_actionTone1.csv" % subject
+    print(src)
+    print(dst)
+
+    src = root_dir + subject + "/%s_actionPress.csv" % subject
+    dst = target_dir + "/%s_actionPress.csv" % subject
     print(src)
     print(dst)
