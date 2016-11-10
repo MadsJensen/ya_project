35796df558bd3ddb837cf64357c90a63485ded5c
Mads Jensen <mje.mads@gmail.com>
2016-11-10 14:30:24 +0100

    fixes

--- IB_analysis/import_preproc_IB_data.py
+++ IB_analysis/import_preproc_IB_data.py
@@ -15,13 +15,15 @@ file_list = glob.glob("subje*")
 subjects = list(set([f[8:11] for f in file_list]))
 subjects.sort()
 
+all_data = pd.DataFrame()
+
 for subject in subjects:
     subject_data = pd.DataFrame()
     for condition in condtions:
         data_file = "subject_%s_%s.csv" % (subject, condition)
         data = pd.read_csv(data_file, delimiter=";")
 
-        if (condition == "singlePress") | (condition == "actionPress1"):
+        if (condition == "singlePress") | (condition == "actionPress"):
             data["recalc_1"] = (data["ansAngle"] < 90) & (
                 data["pressAngle"] > 270)
             data["recalc_2"] = (data["ansAngle"] > 270) & (
@@ -54,3 +56,5 @@ for subject in subjects:
                 data["ansAngle"] - data["toneAngle"]) * 2560 / 360
 
         subject_data = subject_data.append(data, ignore_index=True)
+    
+    all_data = all_data.append(subject_data, ignore_index=True)
